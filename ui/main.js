46531a9440868395f94a37dbcd0d5ef9e465456f
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 18:01:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,23 @@ function moveRight()
 we.onclick=function(){
   var interval=setInterval(moveRight,100);  
 };
+var v1=document.getElementById("b1");
+v1.onclick=function()
+{
+    var re=new XMLHttpRequest();
+    re.onreadystatechange=function()
+    {
+      if(re.readyState==XMLHttpRequest.DONE)
+      {
+          if(re.status==200)
+          {
+              var e=document.getElementById("spa");
+              var response=re.responseText;
+              e.innerHTML=response.toString();
+          }
+      }
+    };
+    re.open('GET','http://divyanshagrawal96.imad.hasura-app.io/counter',true)
+    re.send(null);
+    
+};
