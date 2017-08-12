e294b8ed05b5316c0ca2f7ab7db9fa946d134d5e
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 20:24:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,9 +85,9 @@ app.get('/:articleName',function(req,res)
     res.send(createtemplate(articles[articleName]));
 });
 var names=[];
-app.get('/submit/:name',function(req,res)
+app.get('/submit',function(req,res)
 {
- var name=req.params.name;
+ var name=req.query.name;
  names.push(name);
  res.send(JSON.stringify(names));
 });
