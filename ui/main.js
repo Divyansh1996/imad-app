8c884569642e0c12e09da6956a932712c4c7a7da
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 14:25:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var we=document.getElementById('naruto');
+var move=0;
+function moveRight()
+{
+    move=move+10;
+    we.style.moveRight=move+'px';
+}
+we.onclick=function(){
+  var interval=setInterval(moveRight,100);  
+};
