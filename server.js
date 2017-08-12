e6d5a006be808a22260a90f842c23f85bdf1c08e
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 19:53:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get('/:articleName',function(req,res)
     res.send(createtemplate(articles[articleName]));
 });
 
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
