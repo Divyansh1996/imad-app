51a53cf19c20aac48bfe661d185016090bf69813
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 10:21:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/article-one',function(req,res)
 {
     
-    res.send("This is Article One");
+    res.send(path.join(__dirname,'ui','article-one.html'));
 }
 );
 
