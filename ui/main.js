31a9d9491dd72d36afbd0feb0d5d3007cee6ef89
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 14:26:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var move=0;
 function moveRight()
 {
     move=move+10;
-    we.style.moveRight=move+'px';
+    we.style.marginRight=move+'px';
 }
 we.onclick=function(){
   var interval=setInterval(moveRight,100);  
