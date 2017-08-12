55fe4560626e5b4801441e3fb96011decdbdc5cc
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-08-12 19:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,3 +29,15 @@ v1.onclick=function()
     re.send(null);
     
 };
+b2.onclick=function()
+{
+    var names=['name1','name2','name3'];
+    var list;
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    namelist.innerHTML=list;
+    
+};
