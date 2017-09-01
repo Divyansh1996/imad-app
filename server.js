855f66ac9d197f8f0e1359e7d2b6a6f2a2d93fe0
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-09-01 19:57:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var config={
+    user:'divyanshagrawal96',
+    database:'divyanshagrawal96',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+};
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+   pool.query("SELECT * FROM test",function(err,result){
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON.stringify(result.row));
+       }
+   }); 
+});
 var articleOne={
     title:'Article Kanha',
     heading:'Thts me',
