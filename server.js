5858b87129464ba57a5995d6c8e942d62526f708
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-09-01 21:01:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,29 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
+app.get('/articles/:articleName',function(req,res){
+    pool.query("select * from article where title='"+req.params.articleName +"'" ,function(req,res){
+        
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length===0)
+            {
+                res.status(404).send("Not Found");
+            }
+            else
+            {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
+});
 var counter=0;
 app.get('/counter',function(req,res)
 {
@@ -108,29 +131,7 @@ app.get('/test-db',function(req,res){
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var pool=new Pool(config);
-app.get('/articles/:articleName',function(req,res){
-    pool.query("select * from article where title='"+req.params.articleName +"'" ,function(req,res){
-        
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-            if(result.rows.length===0)
-            {
-                res.status(404).send("Not Found");
-            }
-            else
-            {
-                var articleData=result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-        }
-    });
-    
-});
+
 app.get('/:articleName',function(req,res)
 {
     
