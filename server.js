b89cb69f2f2f6cd025bcc882be4675d5af0f419d
Divyansh1996 <divyanshagrawal96@gmail.com>
2017-09-01 20:59:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,8 +104,32 @@ app.get('/test-db',function(req,res){
        }
    }); 
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+var pool=new Pool(config);
+app.get('/articles/:articleName',function(req,res){
+    pool.query("select * from article where title='"+req.params.articleName +"'" ,function(req,res){
+        
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length===0)
+            {
+                res.status(404).send("Not Found");
+            }
+            else
+            {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
 });
 app.get('/:articleName',function(req,res)
 {
